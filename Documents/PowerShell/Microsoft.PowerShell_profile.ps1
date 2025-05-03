8b643e03019a1259ccaec3ea0dba1486c050b680
Jose Antonio Penedo <jpenedo@marineinstruments.es>
2025-05-04 00:37:10 +0200

    Powershell, remove lf, added y function for Yazi go to dir after exit

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -24,43 +24,6 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\pure.omp.json" | Invoke-Exp
 Set-Alias lvim 'C:\Users\jpenedo\.local\bin\lvim.ps1'
 
 
-# LF FILE MANAGER
-#
-#
-# Autocompletion for powershell.
-#
-# You need to either copy the content of this file to $PROFILE or call this
-# script directly.
-#
-
-Register-ArgumentCompleter -Native -CommandName 'lf' -ScriptBlock {
-    param($wordToComplete)
-    $completions = @(
-        [CompletionResult]::new('-command ', '-command', [CompletionResultType]::ParameterName, 'command to execute on client initialization')
-        [CompletionResult]::new('-config ', '-config', [CompletionResultType]::ParameterName, 'path to the config file (instead of the usual paths)')
-        [CompletionResult]::new('-cpuprofile ', '-cpuprofile', [CompletionResultType]::ParameterName, 'path to the file to write the CPU profile')
-        [CompletionResult]::new('-doc', '-doc', [CompletionResultType]::ParameterName, 'show documentation')
-        [CompletionResult]::new('-last-dir-path ', '-last-dir-path', [CompletionResultType]::ParameterName, 'path to the file to write the last dir on exit (to use for cd)')
-        [CompletionResult]::new('-log ', '-log', [CompletionResultType]::ParameterName, 'path to the log file to write messages')
-        [CompletionResult]::new('-memprofile ', '-memprofile', [CompletionResultType]::ParameterName, 'path to the file to write the memory profile')
-        [CompletionResult]::new('-print-last-dir', '-print-last-dir', [CompletionResultType]::ParameterName, 'print the last dir to stdout on exit (to use for cd)')
-        [CompletionResult]::new('-print-selection', '-print-selection', [CompletionResultType]::ParameterName, 'print the selected files to stdout on open (to use as open file dialog)')
-        [CompletionResult]::new('-remote ', '-remote', [CompletionResultType]::ParameterName, 'send remote command to server')
-        [CompletionResult]::new('-selection-path ', '-selection-path', [CompletionResultType]::ParameterName, 'path to the file to write selected files on open (to use as open file dialog)')
-        [CompletionResult]::new('-server', '-server', [CompletionResultType]::ParameterName, 'start server (automatic)')
-        [CompletionResult]::new('-single', '-single', [CompletionResultType]::ParameterName, 'start a client without server')
-        [CompletionResult]::new('-version', '-version', [CompletionResultType]::ParameterName, 'show version')
-        [CompletionResult]::new('-help', '-help', [CompletionResultType]::ParameterName, 'show help')
-    )
-
-    if ($wordToComplete.StartsWith('-')) {
-        $completions.Where{ $_.CompletionText -like "$wordToComplete*" } | Sort-Object -Property ListItemText
-    }
-}
-
-Function f_lfcd { C:\Users\jpenedo\AppData\Local\Microsoft\WinGet\Packages\gokcehan.lf_Microsoft.Winget.Source_8wekyb3d8bbwe\lf.exe -print-last-dir . | set-location }
-Set-Alias -name lfcd -value f_lfcd
-
 Function f_pistol { wsl ~/go/bin/pistol }
 Set-Alias -name pistol -value f_pistol
 
@@ -78,6 +41,15 @@ function lgd {
     $env:GIT_WORK_TREE = "$HOME"
     lazygit
     Remove-Item Env:\GIT_DIR, Env:\GIT_WORK_TREE
+}
+function y {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+    }
+    Remove-Item -Path $tmp
 }
 
 $env:YAZI_FILE_ONE  = "C:\Program Files\Git\usr\bin\file.exe"
