0aa24156ac95e12c078da17dbbe744b37a3bb055
Jose Antonio Penedo <jpenedo@marineinstruments.es>
2025-05-04 21:53:13 +0200

    Yazi, added plugins,

--- AppData/Roaming/yazi/config/init.lua
+++ AppData/Roaming/yazi/config/init.lua
@@ -1,2 +1,20 @@
 require("zoxide"):setup({update_db = true})
 require("relative-motions"):setup({ show_numbers="relative", show_motion = true, enter_mode ="first" })
+require("yaziline"):setup()
+require("mime-ext"):setup {
+	-- Expand the existing filename database (lowercase), for example:
+	with_files = {
+		makefile = "text/makefile",
+		-- ...
+	},
+
+	-- Expand the existing extension database (lowercase), for example:
+	with_exts = {
+		mk = "text/makefile",
+		-- ...
+	},
+
+	-- If the mime-type is not in both filename and extension databases,
+	-- then fallback to Yazi's preset `mime` plugin, which uses `file(1)`
+	fallback_file1 = false,
+}
