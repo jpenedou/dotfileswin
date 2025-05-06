9a58533acfb4c00f363eb53294e60044bb92c651
Jose Antonio Penedo <jpenedo@marineinstruments.es>
2025-05-06 20:29:26 +0200

    LazyVim, added vscode extra and new binding gr to go references into vscode

--- AppData/Local/nvim/lua/config/keymaps.lua
+++ AppData/Local/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+if vim.g.vscode then
+  local opts = { desc = "Find References (VSCode)" }
+  vim.keymap.set("n", "gr", function()
+    vim.cmd('call VSCodeNotify("editor.action.referenceSearch.trigger")')
+  end, opts)
+
+  vim.keymap.set("v", "gr", function()
+    vim.cmd('call VSCodeNotify("editor.action.referenceSearch.trigger")')
+  end, opts)
+end
