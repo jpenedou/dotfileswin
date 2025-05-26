5e9e41003cfbb3ea8cbc707da4f64413f43c9754
Jose Antonio Penedo <jpenedo@marineinstruments.es>
2025-05-26 22:48:57 +0200

    Powershell, added cat alias for bat

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -52,6 +52,7 @@ $env:YAZI_FILE_ONE  = "C:\Program Files\Git\usr\bin\file.exe"
 Set-Alias -Name yz -Value yazi
 Set-Alias -Name v -Value nvim
 Set-Alias -Name lg -Value lazygit
+Set-Alias cat bat
 
 # Para ouch.exe
 # Nota: se ha compilado con
