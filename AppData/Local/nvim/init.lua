a59262bca0667bb6c56f96ff9152ca23b34c740c
Jose Antonio Penedo <jpenedo@marineinstruments.es>
2025-12-12 12:15:20 +0100

    LazyVim, added c compiler path to fix treesitter compilation parsers

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,2 +1,7 @@
+-- Set CC environment variable for tree-sitter compilation
+vim.env.PATH = "C:/tools/msys64/mingw64/bin;" .. vim.env.PATH
+vim.env.CC = "gcc"
+vim.env.CXX = "g++"
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
