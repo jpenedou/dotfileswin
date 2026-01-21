966e26aa3147d86b7c2344a7dd5df57931750eb6
Jose Antonio Penedo <jpenedo@marineinstruments.es>
2026-01-21 17:31:48 +0100

    LazyGit, Gitlab merge request remotes fix

--- AppData/Local/lazygit/list-remote-upstream-branches.ps1
+++ AppData/Local/lazygit/list-remote-upstream-branches.ps1
@@ -14,12 +14,16 @@ if ($parts.Length -lt 2) { exit 1 }
 $remote = $parts[0]
 $remoteBranch = $parts[1..($parts.Length-1)] -join '/'
 
-git branch -r --format='%(refname:short)' |
+$branches = git branch -r --format='%(refname:short)' |
     Where-Object { $_ -and $_.StartsWith($remote + '/') } |
     ForEach-Object {
         $name = $_.Substring($remote.Length + 1)
         if ($name -ne $remoteBranch) {
-            Write-Output ($remote + '/' + $name)
+            $remote + '/' + $name
         }
     } |
     Sort-Object
+
+if ($branches) {
+    [Console]::Write(($branches -join "`n") + "`n")
+}
