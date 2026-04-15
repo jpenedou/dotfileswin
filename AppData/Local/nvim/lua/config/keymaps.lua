510565fb81f4394a63f63b28e343a980fd329142
Jose Antonio Penedo <jpenedo@marineinstruments.es>
2026-04-15 14:00:57 +0200

    nvim, added A-h A-l to change buffer

--- AppData/Local/nvim/lua/config/keymaps.lua
+++ AppData/Local/nvim/lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<A-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+vim.keymap.set("n", "<A-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 if vim.g.vscode then
   local opts = { desc = "Find References (VSCode)" }
   vim.keymap.set("n", "gr", function()
